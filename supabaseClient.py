59f26c392099f722425d6b1af27fd0d621d8f92b
sathwik-234 <12213095@nitkkr.ac.in>
2024-12-29 09:42:32 +0530

    refactor: switch from dotenv to Streamlit secrets for Supabase configuration

--- supabaseClient.py
+++ supabaseClient.py
@@ -1,9 +1,10 @@
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
 from supabase import create_client, Client
+import streamlit as st
 
-load_dotenv()
+# load_dotenv()
 
-url: str = os.getenv("SUPABASE_URL")
-key: str = os.getenv("SUPABASE_KEY")
+url: str = st.secrets["SUPABASE_URL"]
+key: str = st.secrets["SUPABASE_KEY"]
 supabase: Client = create_client(url, key)
