4de0cb286cdeda54a42294479e6684ca50d847eb
sathwik-234 <12213095@nitkkr.ac.in>
2025-01-07 03:20:56 +0530

    refactor: remove dotenv dependency and use Streamlit secrets for Supabase configuration

--- supabaseClient.py
+++ supabaseClient.py
@@ -1,10 +1,8 @@
-from dotenv import load_dotenv
-import os
+
 from supabase import create_client, Client
 import streamlit as st
 
 
-load_dotenv()
 # url = os.getenv("SUPABASE_URL")
 # key = os.getenv("SUPABASE_KEY")
 url: str = st.secrets["SUPABASE_URL"]
